3d0626b692c9b93cd2e77fb7ea37a839e0902f37
LukyVj <lucas.bonomi@gmail.com>
2020-12-09 16:49:41 +0100

    update variables

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,10 @@ import * as React from "react";
 export interface FontizerProps extends React.Props<Fontizer> {
   position: "bl" | "br" | "tl" | "tr";
   callback: Function;
+  variables?: {
+    size: string;
+    weight: string;
+  };
 }
 
 declare class Fontizer extends React.Component<FontizerProps, any> {}
